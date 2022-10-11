625e2f500ec3dafd0c519352d6f49c64c240f48b
leonardo <leonardo2322@gmail.com>
2022-10-10 20:44:49 -0500

    backend

---
+++ TuringSistems/urls.py
@@ -0,0 +1,15 @@
+
+from django.contrib import admin
+from django.urls import path, include, re_path
+from django.views.static import serve
+from django.views.generic import TemplateView
+from django.conf import settings
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('',TemplateView.as_view(template_name='index.html')),
+    # path('', include('Users.urls'))
+]
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT})
+]
