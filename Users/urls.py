6bf70a3cc92b9373bbc80f132eef2ed29d34c72e
leonardo <leonardo2322@gmail.com>
2022-10-15 17:17:32 -0500

    saludando con super usuario y models

--- Users/urls.py
+++ Users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
+from .views import helloWorld
 
 urlpatterns= [
-
+    path('api/', helloWorld.as_view())
     
 ]
--- Users/views.py
+++ Users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+import imp
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
+
+class helloWorld(APIView):
+    def get(self, request, format=None):
+        hi = ['hello world','fucking world']
+        return Response({'message':'success','quer':hi})
